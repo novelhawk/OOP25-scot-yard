02e348e8e7d19ccfbb61861885df0dcf19ca4066
Raffaello Fraboni <raffaello.fraboni@studio.unibo.it>
2026-01-18 14:03:39 +0100

    docs: add CommandRouter documentation

--- src/main/java/it/unibo/scotyard/model/router/CommandRouter.java
+++ src/main/java/it/unibo/scotyard/model/router/CommandRouter.java
@@ -6,14 +6,26 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 
+/**
+ * Stores a handler for each GameCommand.
+ * This implementation supports at most one handler per GameCommand.
+ */
 public class CommandRouter implements CommandDispatcher, CommandHandlerStore {
     private final Map<Class<?>, Consumer<?>> handlers = new HashMap<>();
+
+    public CommandRouter() {}
 
     @Override
     public <T extends GameCommand> void register(final Class<T> type, final Consumer<T> handler) {
         handlers.put(type, handler);
     }
 
+    /**
+     * Registers a new handler for the GameCommand removing any previous registered handlers.
+     *
+     * @param command the command to dispatch
+     * @param <T> the type of the command to dispatch
+     */
     @SuppressWarnings("unchecked")
     @Override
     public <T extends GameCommand> void dispatch(final T command) {
