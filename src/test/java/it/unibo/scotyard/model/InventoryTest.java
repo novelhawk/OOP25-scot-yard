3cc3d876375d2ce8e6e362216888f1ca25e1e3b4
Raffaello Fraboni <raffaello.fraboni@studio.unibo.it>
2026-01-31 22:38:11 +0100

    test: fix build

--- src/test/java/it/unibo/scotyard/model/InventoryTest.java
+++ src/test/java/it/unibo/scotyard/model/InventoryTest.java
@@ -2,7 +2,7 @@ package it.unibo.scotyard.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import it.unibo.scotyard.commons.Constants;
+import it.unibo.scotyard.commons.patterns.MagicNumbers;
 import it.unibo.scotyard.model.inventory.Inventory;
 import it.unibo.scotyard.model.inventory.InventoryImpl;
 import it.unibo.scotyard.model.map.NodeId;
@@ -19,33 +19,33 @@ public class InventoryTest {
     public void testInitializeDetective() {
         final Inventory inventory = new InventoryImpl();
         inventory.initialize(new Detective(node));
-        assertEquals(inventory.getNumberTickets(TicketType.TAXI), Constants.NUMBER_TICKETS_TAXI);
-        assertEquals(inventory.getNumberTickets(TicketType.BUS), Constants.NUMBER_TICKETS_BUS);
-        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), Constants.NUMBER_TICKETS_UNDERGROUND);
-        assertEquals(inventory.getNumberTickets(TicketType.BLACK), Constants.NONE);
-        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), Constants.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.TAXI), MagicNumbers.NUMBER_TICKETS_TAXI);
+        assertEquals(inventory.getNumberTickets(TicketType.BUS), MagicNumbers.NUMBER_TICKETS_BUS);
+        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), MagicNumbers.NUMBER_TICKETS_UNDERGROUND);
+        assertEquals(inventory.getNumberTickets(TicketType.BLACK), MagicNumbers.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), MagicNumbers.NONE);
     }
 
     @Test
     public void testInitializeBobby() {
         final Inventory inventory = new InventoryImpl();
         inventory.initialize(new Bobby(node));
-        assertEquals(inventory.getNumberTickets(TicketType.TAXI), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.BUS), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.BLACK), Constants.NONE);
-        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), Constants.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.TAXI), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.BUS), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.BLACK), MagicNumbers.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), MagicNumbers.NONE);
     }
 
     @Test
     public void testInitializeMisterX() {
         final Inventory inventory = new InventoryImpl();
         inventory.initialize(new MisterX(node));
-        assertEquals(inventory.getNumberTickets(TicketType.TAXI), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.BUS), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), Constants.INFINITE);
-        assertEquals(inventory.getNumberTickets(TicketType.BLACK), Constants.NUMBER_TICKETS_BLACK);
-        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), Constants.NUMBER_TICKETS_DOUBLE_MOVE);
+        assertEquals(inventory.getNumberTickets(TicketType.TAXI), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.BUS), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), MagicNumbers.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.BLACK), MagicNumbers.NUMBER_TICKETS_BLACK);
+        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), MagicNumbers.NUMBER_TICKETS_DOUBLE_MOVE);
     }
 
     @Test
@@ -53,18 +53,18 @@ public class InventoryTest {
         final Inventory inventory = new InventoryImpl();
         inventory.initialize(new Detective(node));
         inventory.decrementTickets(TicketType.TAXI);
-        assertEquals(inventory.getNumberTickets(TicketType.TAXI), Constants.NUMBER_TICKETS_TAXI - 1);
+        assertEquals(inventory.getNumberTickets(TicketType.TAXI), MagicNumbers.NUMBER_TICKETS_TAXI - 1);
         inventory.decrementTickets(TicketType.BUS);
         inventory.decrementTickets(TicketType.UNDERGROUND);
-        assertEquals(inventory.getNumberTickets(TicketType.BUS), Constants.NUMBER_TICKETS_BUS - 1);
-        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), Constants.NUMBER_TICKETS_UNDERGROUND - 1);
+        assertEquals(inventory.getNumberTickets(TicketType.BUS), MagicNumbers.NUMBER_TICKETS_BUS - 1);
+        assertEquals(inventory.getNumberTickets(TicketType.UNDERGROUND), MagicNumbers.NUMBER_TICKETS_UNDERGROUND - 1);
         inventory.decrementTickets(TicketType.BLACK);
-        assertEquals(inventory.getNumberTickets(TicketType.BLACK), Constants.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.BLACK), MagicNumbers.NONE);
         inventory.decrementTickets(TicketType.DOUBLE_MOVE);
-        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), Constants.NONE);
+        assertEquals(inventory.getNumberTickets(TicketType.DOUBLE_MOVE), MagicNumbers.NONE);
         inventory.initialize(new MisterX(node));
         inventory.decrementTickets(TicketType.TAXI);
-        assertEquals(inventory.getNumberTickets(TicketType.TAXI), Constants.INFINITE);
+        assertEquals(inventory.getNumberTickets(TicketType.TAXI), MagicNumbers.INFINITE);
     }
 
     @Test
@@ -76,11 +76,11 @@ public class InventoryTest {
         assertTrue(inventory.containsTicket(TicketType.UNDERGROUND));
         assertTrue(inventory.containsTicket(TicketType.BLACK));
         assertTrue(inventory.containsTicket(TicketType.DOUBLE_MOVE));
-        for (int i = 0; i < Constants.NUMBER_TICKETS_DOUBLE_MOVE; i++) {
+        for (int i = 0; i < MagicNumbers.NUMBER_TICKETS_DOUBLE_MOVE; i++) {
             inventory.decrementTickets(TicketType.DOUBLE_MOVE);
         }
         assertFalse(inventory.containsTicket(TicketType.DOUBLE_MOVE));
-        for (int i = 0; i < Constants.NUMBER_TICKETS_BLACK; i++) {
+        for (int i = 0; i < MagicNumbers.NUMBER_TICKETS_BLACK; i++) {
             inventory.decrementTickets(TicketType.BLACK);
         }
         assertFalse(inventory.containsTicket(TicketType.BLACK));
