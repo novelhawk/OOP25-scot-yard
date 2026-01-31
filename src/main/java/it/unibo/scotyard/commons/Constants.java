3127233a79e874b72c94b0f9a5075481968bafdc
Irene Borri <irene.borri@studio.unibo.it>
2026-01-31 22:56:33 +0100

    Constants change

--- src/main/java/it/unibo/scotyard/commons/Constants.java
+++ src/main/java/it/unibo/scotyard/commons/Constants.java
@@ -11,7 +11,6 @@ public final class Constants {
     public static final List<Size> RESOLUTIONS = List.of(
             Size.of(3840, 2160), Size.of(3440, 1440), Size.of(2560, 1440), Size.of(1920, 1080), Size.of(1280, 720));
 
-
     // Reveal turns for Mister X
     public static final List<Integer> REVEAL_TURNS_MISTER_X = List.of(3, 8, 13, 18);
 
