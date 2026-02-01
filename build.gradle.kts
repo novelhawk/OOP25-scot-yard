f6cfc2a3b6f586847aac48eed78be1d158e0c084
Raffaello Fraboni <raffaello.fraboni@studio.unibo.it>
2026-02-01 23:24:44 +0100

    build: remove gradle-java-qa to allow building

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     application
 
     id("com.diffplug.spotless") version "8.1.0"
-    id("org.danilopianini.gradle-java-qa") version "1.159.0"
     id("com.gradleup.shadow") version "9.3.1"
 }
 
